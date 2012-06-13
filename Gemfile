b8609e028ef0f75e217cee86f4c7b7006550c16f
Lee Martin <leepaulmartin@gmail.com>
2012-06-13 15:37:21 -0700

    let's git it on

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'http://rubygems.org'
+
+gem 'haml'
+gem 'sinatra'
+gem 'mini_magick'
+gem 'rmagick'
+gem 'json'
---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'haml'
+require 'cgi'
+require 'net/https'
+require "rmagick"
+require 'json'
+
+# require 'sass/plugin/rack'
+# use Sass::Plugin::Rack
+
+set :protection, :except => :json_csrf
+
+get '/' do
+  haml :index
+end
+
+get '/w*' do
+  content_type :json
+
+  waveform = []
+
+  image = Magick::Image.read(params[:url]).first
+  image.crop!(0, 0, image.columns, image.rows / 2)
+  image.rotate!(90)
+
+  columns = image.columns
+
+  image.each_pixel do |pixel, c, r|
+    if waveform.length <= r && (pixel.opacity == 0 || c == columns - 1)
+      waveform << c / columns.to_f
+    end
+  end
+
+  "#{ params[:callback] }(#{ waveform.to_json });"
+end
