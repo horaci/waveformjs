1896ea8873f90a07c344dfdc4c16275c1444edb7
Johannes Wagener <johannes@wagener.cc>
2012-06-19 14:22:49 -0700

    animate header waveform

--- public/application.js
+++ public/application.js
@@ -1,24 +1,52 @@
-(function() {
-
-  $(function() {
-    var a, d, i, v, _i, _j, _len;
-    d = [];
-    a = [0, 0.25, 0, 1, 0, 0.5, 0, 0.75];
-    for (_i = 0, _len = a.length; _i < _len; _i++) {
-      v = a[_i];
-      for (i = _j = 0; _j <= 7; i = ++_j) {
-        d.push(v);
-      }
+$(function() {
+  var a, d, i, v, _i, _j, _len;
+  d = [];
+  a = [0, 0.25, 0, 1, 0, 0.5, 0, 0.75];
+  for (_i = 0, _len = a.length; _i < _len; _i++) {
+    v = a[_i];
+    for (i = _j = 0; _j <= 7; i = ++_j) {
+      d.push(v);
     }
-    return new Waveform({
-      container: $("#logo")[0],
-      outerColor: "transparent",
-      innerColor: "#FF6600",
-      width: 56,
-      height: 100,
-      interpolate: false,
-      data: d
-    });
+  }
+
+  var i = 0;
+  var headerWaveform = new Waveform({
+    container: $("#logo")[0],
+    outerColor: "transparent",
+    innerColor: function(x){
+      if(i > x){
+        return "#ff6600";
+      }else{
+        return "#333";
+      }
+    },
+    width: 56,
+    height: 100,
+    interpolate: false,
+    data: d
   });
 
-}).call(this);
+  setTimeout(function(){
+    i = 0.25;
+    headerWaveform.redraw();
+  }, 300);
+
+
+  setTimeout(function(){
+    i = 0.50;
+    headerWaveform.redraw();
+  }, 400);
+
+  setTimeout(function(){
+    i = 0.75;
+    headerWaveform.redraw();
+  }, 500);
+
+
+  setTimeout(function(){
+    i = 1;
+    headerWaveform.redraw();
+  }, 600);
+
+});
+
